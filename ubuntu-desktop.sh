84463fcdf637680afa6798e95a0b0e737022ca35
Tokuhiro Matsuno <tokuhirom@tokuhirom-laptop.(none)>
2010-09-08 06:28:28 +0900

    updated for ubuntu desktop

--- ubuntu-desktop.sh
+++
@@ -1,4 +0,0 @@
-sudo aptitude install -y gnome-do ibus-skk vlc language-pack-ja
-
-wget http://archive.debian-maintainers.org/unofficial/packages/libdvdcss/current_amd64/libdvdcss2_1.2.10-1_amd64.deb
-dpkg -i libdvdcss2_1.2.10-1_amd64.deb
